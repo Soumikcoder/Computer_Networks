5a992bcb8dfb092f9fd48ad5ea85d260628c33d6
Soumik <soumik20020906@gmail.com>
2024-05-26 18:55:51 +0530

    Bug fixed

--- ClassofIP.py
+++ ClassofIP.py
@@ -1,18 +1,20 @@
 import socket as sock
 hostname=sock.gethostname()
-ip_addr='192.168.0. ip1'
+
 print("Your computer Name is :"+hostname)
-print("Your ip Address is:"+ip_addr)
+print("Your ip Address is:"+sock.gethostbyname(hostname))
+ip_addr=input('Enter a ip address:')
 ip=[int(i) for i in ip_addr.split('.')]
 if len(ip) != 4:
     print('Not valid!')
     exit(1)
 ip_bin=[]
+print('Ip in Binary:',end=" ")
 for i in range(len(ip)):
     end_point='.'
     if i == len(ip)-1:
         end_point='\n'
-    ip_bin.append('{0:07b}'.format(ip[i]))
+    ip_bin.append('{0:08b}'.format(ip[i]))
     print(ip_bin[-1],end=end_point)
 
 if ip_bin[0][0]=='0' :
